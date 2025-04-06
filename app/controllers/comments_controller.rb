19cf78f66edd90ad429915f31a5c47f34543901b
Alfonso Uceda <alfonso@hubbado.com>
2025-04-06 22:10:17 +0200

    Comments can be created and are displayed in projects show view

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+class CommentsController < ApplicationController
+  before_action :set_project
+
+  # GET /projects/:project_id/comments/new
+  def new
+  end
+
+  # POST /projects/:project_id/comments
+  def create
+    @comment = @project.comments.build(comment_params)
+
+    if @project.save
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.append(
+              'comments',
+              partial: 'comments/comment',
+              locals: { comment: @comment }
+            ),
+            turbo_stream.replace(
+              'new_comment',
+              partial: 'projects/new_comment'
+            )
+          ]
+        end
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def comment_params
+      params.expect(comment: [ :body ])
+    end
+
+    def set_project
+      @project = Project.find(params.expect(:project_id))
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_06_151844) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_06_155625) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "projects"
 end
